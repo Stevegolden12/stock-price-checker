b1581d48cc0417dad401adbb2c1e24a5c361246b
Steve Golden <sgolden2525@outlook.com>
2019-05-27 00:54:20 -0500

    GET single request
    Added node packages:  axios and dotenv
    
    Added axios fetch to retrieve information
    
    Added .env file to store information.

--- routes/api.js
+++ routes/api.js
@@ -10,14 +10,41 @@
 
 var expect = require('chai').expect;
 var MongoClient = require('mongodb');
+const iex = require('iexcloud_api_wrapper');
+const fetch = require("node-fetch");
+const axios = require('axios')
+require('dotenv').config()
 
 const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
 
 module.exports = function (app) {
 
   app.route('/api/stock-prices')
-    .get(function (req, res) {
-
+    .get(function (req, res) { 
+  
+      axios.get(`https://www.googleapis.com/books/v1/volumes?q=flowersforAlgernon&key=${process.env.KEY}`)
+        .then(function (response) {
+          const title = response.data.items[0].volumeInfo.title
+          const pDate = response.data.items[0].volumeInfo.publishedDate
+          res.send(title + " was published on " + pDate)
+        })      
+        .catch(function (err) {
+          console.log("Error is: " + err)
+        })
+       
+  
     });
 
 };
+
+/* 
+ 
+ axios.all([
+    axios.get('http://google.com'),
+    axios.get('http://apple.com')
+  ])
+  .then(axios.spread((googleRes, appleRes) => {
+    // do something with both responses
+  });
+ 
+ */
